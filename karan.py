adeb803e4ac5ec51a76c74d1a78e66bda9daaa9d
Karan0297 <131270992+Karan0297@users.noreply.github.com>
2023-04-19 20:27:32 +0530

    Update karan.py

--- karan.py
+++ karan.py
@@ -5,7 +5,6 @@ num1 = st.number_input("Enter the first number:")
 num2 = st.number_input("Enter the second number:")
 num3 = st.number_input("Enter the third number:")
 
-def largest_number():
-    if st.button("Find the largest number"):
+if st.button("Find the largest number"):
         largest_num = np.max([num1, num2, num3])
         st.write("The largest number is:", largest_num)
