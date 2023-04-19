e61e07349f0eb9c9149d46925ddd691e2f4a933e
Karan0297 <131270992+Karan0297@users.noreply.github.com>
2023-04-19 20:23:49 +0530

    Update karan.py

--- karan.py
+++ karan.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("Find the largest among the 3 given numbers")
 
-    num1 = st.number_input("Enter the first number:")
-    num2 = st.number_input("Enter the second number:")
-    num3 = st.number_input("Enter the third number:")
+num1 = st.number_input("Enter the first number:")
+num2 = st.number_input("Enter the second number:")
+num3 = st.number_input("Enter the third number:")
 
     def largest_number():
     
