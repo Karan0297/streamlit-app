5ff578238da070659dec92617444490869995502
Karan0297 <131270992+Karan0297@users.noreply.github.com>
2023-04-19 20:23:14 +0530

    Update karan.py

--- karan.py
+++ karan.py
@@ -1,12 +1,15 @@
 import streamlit as st
 st.title("Find the largest among the 3 given numbers")
-def largest_number():
-    
 
-    # Create three input boxes for the three numbers
     num1 = st.number_input("Enter the first number:")
     num2 = st.number_input("Enter the second number:")
     num3 = st.number_input("Enter the third number:")
+
+    def largest_number():
+    
+
+    # Create three input boxes for the three numbers
+
 
     # Create a button to find the largest number
     if st.button("Find the largest number"):
